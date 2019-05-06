999cafa2d6870fba4b66f2e6b6f88cfd685d6403
Eric Hanko <eric.hanko1@gmail.com>
2019-05-06 11:10:39 -0700

    kitchen plugin?

--- kitchen-azure-pipelines.gemspec
+++ kitchen-azure-pipelines.gemspec
@@ -2,10 +2,10 @@ Gem::Specification.new do |s|
   s.name        = 'kitchen-azure-pipelines'
   s.version     = '0.5.0'
   s.date        = '2019-05-06'
-  s.summary     = "Generate an Azure Pipelines build template using kitchen platforms and suites."
-  s.description = "A simple hello world gem"
-  s.authors     = ["Eric Hanko"]
+  s.summary     = 'Generate an Azure Pipelines build template for Kitchen CI.'
+  s.description = 'A simple hello world gem'
+  s.authors     = ['Eric Hanko']
   s.email       = 'eric.hanko1@gmail.com'
-  s.files       = ["azure-pipelines.yml.erb", "azure_pipeline_generator.rb"]
+  s.files       = ['azure-pipelines.yml.erb', 'azure_pipeline_generator.rb']
   s.license     = 'MIT'
 end
---
+++ lib/kitchen/command/azure_pipelines_generator.rb
@@ -0,0 +1,44 @@
+require 'erb'
+require 'yaml'
+require 'kitchen/command'
+
+module Kitchen
+  module Command
+    # Command to generate the azure-pipelines.yml file
+    class AzurePipelineGenerator < Kitchen::Command::Base
+      def call
+        azure_pipelines_erb = AzurePipelineGenerator.render_template
+        File.open(t.name, 'w+') do |f|
+          f.write azure_pipelines_erb
+        end
+      end
+
+      def render_template
+        template = File.join __dir__, 'templates', 'azure-pipelines.yml.erb'
+        content = IO.read template
+        render = ERB.new content,
+                         safe_level = nil,
+                         trim_mode = '-',
+                         eoutvar = '_erbout'
+        render.result binding
+      end
+
+      def kitchen_suites
+        instances['suites']
+      end
+
+      def kitchen_platforms
+        instances['platforms']
+      end
+
+      def kitchen_yaml
+        File.join __dir__, '.kitchen.yml'
+      end
+
+      def instances
+        k = IO.read kitchen_yaml
+        YAML.safe_load k
+      end
+    end
+  end
+end
